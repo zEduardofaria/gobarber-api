9e84278f7917995f49f97256183eb61fd27cf0ae
Eduardo <z.eduardofaria@gmail.com>
2020-01-24 15:17:02 -0300

    Restarting first files

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
-
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
-import FileController from './app/controllers/FileController';
-import ProviderController from './app/controllers/ProviderController';
-import AppointmentController from './app/controllers/AppointmentController';
-import ScheduleController from './app/controllers/ScheduleController';
-import NotificationController from './app/controllers/NotificationController';
-import AvailableController from './app/controllers/AvailableController';
-
-import authMiddleware from './app/middlewares/auth';
-
 const routes = new Router();
 const upload = multer(multerConfig);
 
